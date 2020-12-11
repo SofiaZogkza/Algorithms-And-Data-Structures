3c556fc7b16d25dd6d1b57412fb92e06e0f4ea7a
SofiaZogkza <sof_zog@hotmail.com>
2020-12-11 17:54:27 +0200

    Added: PalindromePermutation

---
+++ AlgorithmsAndDataStructures/ArraysAndStrings/PalindromePermutation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysAndStrings
+{
+    public class PalindromePermutation
+    {
+        public static bool IsPalindrome(string str)
+        {
+            int strLength = str.Length - 1;
+
+            for (int i = 0; i < strLength; i++)
+            {
+                if (str[i] != str[strLength - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsPermutation(string str)
+        {
+            if (str == null) return false;
+            Dictionary<char, int> strDict = new Dictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (strDict.ContainsKey(str[i]))
+                {
+                    strDict[str[i]] = strDict[str[i]] + 1;
+                }
+                else
+                {
+                    strDict[str[i]] = 1;
+                }
+            }
+            var valueOccurs = 0;
+            foreach (var i in strDict)
+            {
+                // EVEN NUMBERS
+                if (strDict.Count % 2 == 0)
+                {
+                    if (strDict[i.Key] % 2 == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                // ODD NUMBERS - One value must be unique, the rest must be 
+                else
+                {
+                    if (strDict[i.Key] % 2 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (valueOccurs > 1) return false;
+                        valueOccurs = valueOccurs + 1;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
--- AlgorithmsAndDataStructures/ArraysAndStrings/Program.cs
+++ AlgorithmsAndDataStructures/ArraysAndStrings/Program.cs
@@ -7,16 +7,28 @@ namespace ArraysAndStrings
         public static void Main(string[] args)
         {
             #region IsUnique
-            Console.WriteLine("Is the string unique? : " + IsUnique.IsUniqueString("sofia"));
-            Console.WriteLine("Is the string unique? : " + IsUnique.IsUniqueString2("softsoftr"));
+            //Console.WriteLine("Is the string unique? : " + IsUnique.IsUniqueString("sofia"));
+            //Console.WriteLine("Is the string unique? : " + IsUnique.IsUniqueString2("softsoftr"));
             #endregion
 
             #region Permutation
-            CheckPermutation.IsPermutation("ssofia","fiasos");
+            //CheckPermutation.IsPermutation("ssofia","fiasos");
             #endregion
 
-            URLify.UrlifyTheString("Your  Name i   o");
+            #region URLify
+            //URLify.UrlifyTheString("Your  Name i   o");
+            #endregion
 
+            #region Palindrome Permutation
+            Console.WriteLine("abcba is Palindrome: " + PalindromePermutation.IsPalindrome("abcba"));
+            Console.WriteLine("abcbba is Palindrome: " + PalindromePermutation.IsPalindrome("abcbba"));
+            Console.WriteLine("abba is Palindrome: " + PalindromePermutation.IsPalindrome("abba") + "\n");
+
+            Console.WriteLine("abba is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
+            Console.WriteLine("abcccba is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
+            Console.WriteLine("abbacc is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
+            Console.WriteLine("abcbba is Palindrome Permutation: " + PalindromePermutation.IsPalindrome("abcbba") + "\n");
+            #endregion
 
             Console.ReadLine();
         }
