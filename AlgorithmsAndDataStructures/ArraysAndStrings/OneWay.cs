42ca59d1e8859d5d70a4460fe1b2528958d079d9
Sofia Zogkza <sof_zog@hotmail.com>
2020-12-26 22:14:34 +0200

    oneway

---
+++ AlgorithmsAndDataStructures/ArraysAndStrings/OneWay.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArraysAndStrings
+{
+    public static class OneWay
+    {
+        public static bool CheckIfOneEdit(string str1, string str2)
+        {
+            Console.WriteLine("String 1 : " + str1 + "\nString 2 : " + str2);
+
+            var dict1 = new Dictionary<char, int>();
+            var dict2 = new Dictionary<char, int>();
+
+            foreach (var s in str1)
+            {
+                if(dict1.ContainsKey(s))
+                {
+                    dict1[s]++;
+                }
+                dict1[s] = 1;
+            }
+
+
+            var loopCount = 0;
+            var replaceCount = 0;
+            if(dict1.Count == str2.Length)
+            {
+                foreach (var item in dict1.Keys.ToList())
+                {                    
+                    if(item == str2[loopCount])
+                    {
+                        dict1[item]--;
+                    }
+                    else
+                    {
+                        replaceCount++;
+                    }
+                    loopCount++;
+                }
+
+                if (replaceCount >= 2)
+                {
+                    Console.WriteLine("There are " + replaceCount+ " edits!");
+                    Console.WriteLine("Returns false!");
+                    return false;
+                }
+                Console.WriteLine("There are " + replaceCount + " edits!");
+                Console.WriteLine("Returns true!");
+                return true;
+            }
+
+
+            foreach (var item in str2)
+            {
+                if (dict1.ContainsKey(item))
+                {
+                    dict1[item]--;
+                }
+                else
+                {
+                    dict1[item] = 1;
+                }                
+            }
+
+            var count = 0;
+            foreach(var item in dict1.Values)
+            {
+                if(item != 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("There are " + count + " edits!");
+
+            if (count >= 2)
+            {
+                Console.WriteLine("Returns false!");
+                return false;
+            }
+            Console.WriteLine("Returns true!");
+            return true;
+        }
+
+    }
+}
--- AlgorithmsAndDataStructures/ArraysAndStrings/Program.cs
+++ AlgorithmsAndDataStructures/ArraysAndStrings/Program.cs
@@ -20,15 +20,22 @@ namespace ArraysAndStrings
             #endregion
 
             #region Palindrome Permutation
-            Console.WriteLine("abcba is Palindrome: " + PalindromePermutation.IsPalindrome("abcba"));
-            Console.WriteLine("abcbba is Palindrome: " + PalindromePermutation.IsPalindrome("abcbba"));
-            Console.WriteLine("abba is Palindrome: " + PalindromePermutation.IsPalindrome("abba") + "\n");
+            //Console.WriteLine("abcba is Palindrome: " + PalindromePermutation.IsPalindrome("abcba"));
+            //Console.WriteLine("abcbba is Palindrome: " + PalindromePermutation.IsPalindrome("abcbba"));
+            //Console.WriteLine("abba is Palindrome: " + PalindromePermutation.IsPalindrome("abba") + "\n");
 
-            Console.WriteLine("abba is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
-            Console.WriteLine("abcccba is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
-            Console.WriteLine("abbacc is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
-            Console.WriteLine("abcbba is Palindrome Permutation: " + PalindromePermutation.IsPalindrome("abcbba") + "\n");
+            //Console.WriteLine("abba is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
+            //Console.WriteLine("abcccba is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
+            //Console.WriteLine("abbacc is Palindrome Permutation: " + PalindromePermutation.IsPermutation("abba"));
+            //Console.WriteLine("abcbba is Palindrome Permutation: " + PalindromePermutation.IsPalindrome("abcbba") + "\n");
             #endregion
+
+            OneWay.CheckIfOneEdit("sofia", "sofia");
+            OneWay.CheckIfOneEdit("sofiazogkza", "sofia");
+            OneWay.CheckIfOneEdit("pale", "ple");
+            OneWay.CheckIfOneEdit("pales", "pale");
+            OneWay.CheckIfOneEdit("pale", "bale");
+            OneWay.CheckIfOneEdit("pale", "bake");
 
             Console.ReadLine();
         }
