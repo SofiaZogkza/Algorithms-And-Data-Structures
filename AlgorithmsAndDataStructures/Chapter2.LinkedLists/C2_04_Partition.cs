034fd4e2b81430ffefbf2f38a05fc68999f6144d
Sofia Zogkza <sof_zog@hotmail.com>
2021-01-19 01:45:11 +0200

    added particion

---
+++ AlgorithmsAndDataStructures/Chapter2.LinkedLists/C2_04_Partition.cs
@@ -0,0 +1,111 @@
+﻿using Chapter2.LinkedLists.HelperMethods;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter2.LinkedLists
+{
+    public class C2_04_Partition
+    {
+        public static Node ParticionTheList_Solution1(Node head, int x)
+        {
+            Console.WriteLine("The list as is now:");
+            PrintList(head);
+
+            Node beforeStart = null;
+            Node beforeEnd = null;
+            Node afterStart = null;
+            Node afterEnd = null;
+
+            while (head != null)
+            {
+                Node next = head.next;
+                head.next = null;
+                if (head.data < x)
+                {
+                    if (beforeStart == null)
+                    {
+                        beforeStart = head;
+                        beforeEnd = beforeStart;
+                    }
+                    else
+                    {
+                        beforeEnd.next = head;
+                        beforeEnd = head;
+                    }
+                }
+                else
+                {
+                    if (afterStart == null)
+                    {
+                        afterStart = head;
+                        afterEnd = afterStart;
+                    }
+                    else
+                    {
+                        afterEnd.next = head;
+                        afterEnd = head;
+                    }
+                }
+                head = next;
+            }
+
+            if (beforeStart == null)
+            {
+                return afterStart;
+            }
+
+            /* Merge before list and after list */
+            beforeEnd.next = afterStart;
+
+            Console.WriteLine("\nThe final list:");
+            PrintList(beforeStart);
+
+            return beforeStart;
+        }
+
+        public static Node ParticionTheList_Solution2(Node start, int x)
+        {
+            Console.WriteLine("The list as is now:");
+            PrintList(start);
+
+            Node head = start;
+            Node tail = start;
+
+            while (start != null)
+            {
+                Node next = start.next;
+                if (start.data < x)
+                {
+                    // Insert node at head.
+                    start.next = head;
+                    head = start;
+                }
+                else
+                {
+                    // Insert node at tail.
+                    tail.next = start;
+                    tail = start;
+                }
+                start = next;
+            }
+            tail.next = null;
+
+            Console.WriteLine("\nThe final list:");
+            PrintList(head);
+
+            // The head has changed, so we need to return it to the user.
+            return head;
+        }
+
+        public static void PrintList(Node head)
+        {
+            var temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }    
+}
--- AlgorithmsAndDataStructures/Chapter2.LinkedLists/Program.cs
+++ AlgorithmsAndDataStructures/Chapter2.LinkedLists/Program.cs
@@ -12,13 +12,13 @@ namespace Chapter2.LinkedLists
             #region Remove Dublicates
             //C2_01_RemoveDups.RemoveDuplicates(10);
 
-            Node start = new Node(10);
-            start.next = new Node(12);
-            start.next.next = new Node(11);
-            start.next.next.next = new Node(11);
-            start.next.next.next.next = new Node(12);
-            start.next.next.next.next.next = new Node(11);
-            start.next.next.next.next.next.next = new Node(10);
+            //Node start = new Node(10);
+            //start.next = new Node(12);
+            //start.next.next = new Node(11);
+            //start.next.next.next = new Node(11);
+            //start.next.next.next.next = new Node(12);
+            //start.next.next.next.next.next = new Node(11);
+            //start.next.next.next.next.next.next = new Node(10);
 
             //Console.WriteLine("Linked list before removing " + "duplicates :");
             //help.PrintList(start);
@@ -32,8 +32,17 @@ namespace Chapter2.LinkedLists
 
             //C2_02_ReturnKthToLast.ReturnKthToLast(start, 3);
 
-            C2_03_DeleteMiddleNode.DeleteNode(start);
+            //C2_03_DeleteMiddleNode.DeleteNode(start);
 
+            Node start = new Node(3);
+            start.next = new Node(5);
+            start.next.next = new Node(8);
+            start.next.next.next = new Node(5);
+            start.next.next.next.next = new Node(10);
+            start.next.next.next.next.next = new Node(2);
+            start.next.next.next.next.next.next = new Node(1);
+            //C2_04_Partition.ParticionTheList_Solution1(start, 5);
+            C2_04_Partition.ParticionTheList_Solution2(start, 5);
 
 
             Console.ReadLine();
